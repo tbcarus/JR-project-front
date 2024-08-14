b39ce5783e308e13d8497f52fdd1b9ab44b251f7
tbcarus <70680359+tbcarus@users.noreply.github.com>
2024-08-14 06:57:06 +0300

    Up to 18 points

--- src/main/webapp/js/rpg.js
+++ src/main/webapp/js/rpg.js
@@ -21,6 +21,7 @@ function fillTable(players) {
     mainDiv.innerHTML = '';
     for (let player of players) {
         let row = document.createElement("div");
+        row.id = player.id;
         row.style.display = "flex";
 
         let colNumber = document.createElement("div");
@@ -63,23 +64,139 @@ function fillTable(players) {
         colBanned.style.width = "10%";
         colBanned.innerHTML = player.banned;
         row.append(colBanned);
-        mainDiv.append(row);
 
         let colEdit = document.createElement("div");
         colEdit.style.width = "10%";
-        colEdit.innerHTML = '<img src="img/edit.png" style="width: 25px">';
+        colEdit.innerHTML = '<img src="img/edit.png" style="width: 25px; cursor: pointer">';
+        colEdit.onclick = function () {
+            editRow(row.id, player);
+        };
         row.append(colEdit);
-        mainDiv.append(row);
 
         let colDelete = document.createElement("div");
         colDelete.style.width = "10%";
-        colDelete.innerHTML = '<img src="img/delete.png" style="width: 25px">';
+        colDelete.innerHTML = '<img src="img/delete.png" style="width: 25px; cursor: pointer">';
         colDelete.onclick = function () {
             deletePlayer(player.id);
         };
         row.append(colDelete);
+
         mainDiv.append(row);
     }
+}
+
+async function editRow(rowId, player) {
+    let row = document.getElementById(rowId);
+    row.innerHTML = '';
+
+    let colNumber = document.createElement("div");
+    colNumber.style.width = "2%";
+    colNumber.innerHTML = player.id;
+    row.append(colNumber);
+
+    let colName = document.createElement("div");
+    colName.style.width = "10%";
+    let inputName = document.createElement('input');
+    inputName.name = 'name';
+    inputName.value = player.name;
+    inputName.style.width = '100px';
+    colName.append(inputName);
+    row.append(colName);
+
+    let colTitle = document.createElement("div");
+    colTitle.style.width = "18%";
+    let inputTitle = document.createElement('input');
+    inputTitle.name = 'title';
+    inputTitle.value = player.title;
+    inputTitle.style.width = '100px';
+    colTitle.append(inputTitle);
+    row.append(colTitle);
+
+    let colRace = document.createElement("div");
+    colRace.style.width = "10%";
+    let selectRace = document.createElement('select');
+    selectRace.name = 'race';
+    const enumRace = {
+        HUMAN: 'HUMAN', DWARF: 'DWARF', ELF: 'ELF', GIANT: 'GIANT', ORC: 'ORC', TROLL: 'TROLL', HOBBIT: 'HOBBIT'
+    }
+    for (let race in enumRace) {
+        let opt = document.createElement('option');
+        opt.value = race;
+        opt.text = race;
+        if (race === player.race) {
+            opt.selected = 'selected';
+        }
+        selectRace.append(opt);
+    }
+    colRace.append(selectRace);
+    row.append(colRace);
+
+    let colProfession = document.createElement("div");
+    colProfession.style.width = "10%";
+    let selectProf = document.createElement('select');
+    selectProf.name = 'profession';
+    const enumProf = {
+        WARRIOR: 'WARRIOR',
+        ROGUE: 'ROGUE',
+        SORCERER: 'SORCERER',
+        CLERIC: 'CLERIC',
+        PALADIN: 'PALADIN',
+        NAZGUL: 'NAZGUL',
+        WARLOCK: 'WARLOCK',
+        DRUID: 'DRUID'
+    }
+    for (let prof in enumProf) {
+        let opt = document.createElement('option');
+        opt.value = prof;
+        opt.text = prof;
+        if (prof === player.profession) {
+            opt.selected = 'selected';
+        }
+        selectProf.append(opt);
+    }
+    colProfession.append(selectProf);
+    row.append(colProfession);
+
+    let colLevel = document.createElement("div");
+    colLevel.style.width = "10%";
+    colLevel.innerHTML = player.level;
+    row.append(colLevel);
+
+    let colBirthday = document.createElement("div");
+    colBirthday.style.width = "10%";
+    let date = new Date(player.birthday);
+    colBirthday.innerHTML = date.getMonth() + 2 + '/' + date.getDate() + '/' + date.getFullYear();
+    row.append(colBirthday);
+
+    let colBanned = document.createElement("div");
+    colBanned.style.width = "10%";
+    let selectBan = document.createElement('select');
+    selectBan.name = 'banned';
+    const enumBan = {true: true, false: false}
+    for (let ban in enumBan) {
+        let opt = document.createElement('option');
+        opt.value = ban === 'true';
+        opt.text = ban === 'true';
+        if (player.banned === (ban === 'true')) {
+            opt.selected = 'selected';
+        }
+        selectBan.append(opt);
+    }
+    colBanned.append(selectBan);
+    row.append(colBanned);
+
+    let colEdit = document.createElement("div");
+    colEdit.style.width = "10%";
+    colEdit.innerHTML = '<img src="img/save.png" style="width: 25px; cursor: pointer">';
+    colEdit.onclick = function () {
+        // TODO POST
+    };
+    row.append(colEdit);
+
+    let colDelete = document.createElement("div");
+    colDelete.style.width = "10%";
+    row.append(colDelete);
+
 }
 
 async function deletePlayer(playerId) {
